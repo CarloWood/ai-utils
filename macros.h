80787d6b8a42872765ab15722a61f177f24944f9
Carlo Wood <carlo@alinoe.com>
2017-01-04 00:07:14 +0100

    Add UNUSED_ARG macro.

--- macros.h
+++ macros.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #if defined(__GNUC__) && __GNUC__ >= 3
 #define AI_LIKELY(EXPR) __builtin_expect (!!(EXPR), true)
 #define AI_UNLIKELY(EXPR) __builtin_expect (!!(EXPR), false)
@@ -5,3 +7,5 @@
 #define AI_LIKELY(EXPR) (EXPR)
 #define AI_UNLIKELY(EXPR) (EXPR)
 #endif
+
+#define UNUSED_ARG(x)
