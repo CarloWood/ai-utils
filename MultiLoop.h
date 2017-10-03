31137576d0df5eb32b16d756999ab30639aff8bf
Carlo Wood <carlo@alinoe.com>
2017-10-04 01:46:54 +0200

    Add support for End of Loop code, and different begin values.

--- MultiLoop.h
+++ MultiLoop.h
@@ -31,13 +31,15 @@
 //
 // // Have 23 nested for loops.
 // for (MultiLoop ml(23); !ml.finished(); ml.next_loop())
+// {
 //   // No code should go here!
-//   for (; ml() < 3; ++ml)	// Each loops runs from 0 to 3 (in this case).
+//   while (ml() < 3)	        // Each loops runs from 0 to 3 (in this case).
 //   {
 //     // Here we are at the top of loop *ml with value ml().
 //     // The values of (previous) loop counters are available through ml[0..*ml]
 //     // (ml() returns ml[*ml]).
 //
+//     // Beginning of loop code (all loops).
 //     if (*ml == 7 && ml[5] == 2 && ml() == 1)         // We're at the top of loop number 7 (the 8th loop),
 //                                                      // the value of loop #5 is 2 and the current loop (loop 7)
 //     							// has value 1.
@@ -61,6 +63,8 @@
 //
 //     if (ml.inner_loop())		// Most inner loop.
 //     {
+//       // Inner loop body.
+//
 //       if (...)
 //         continue;			// Normal continue of inner loop;
 //         				// Calling ml.breaks(0); break; is allowed too.
@@ -73,14 +77,29 @@
 //         ml.breaks(5);		// break out of 5 loops.
 //         break;			// Return control to MultiLoop.
 //       }
+//
+//       // End of inner loop.
 //     }
+//     // Loop start values.
+//     ml.start_next_loop_at(ml());     // This causes each loop to begin at with the value of the previous loop.
 //   }
-//   // No code should go here!
+//   if (ml.end_of_loop())
+//   {
+//     // End of loop code for all loops except the inner loop.
+//   }
+// }
 
 class MultiLoop {
   public:
     // Construct a MultiLoop of n loops.
-    MultiLoop(unsigned int n) : M_loops(n), M_counters(new int[n + 1]), M_current_loop(n > 0 ? 1 : 0) { M_counters[M_current_loop] = 0; }
+    explicit MultiLoop(unsigned int n, int b = 0) :
+      M_loops(n),
+      M_counters(new int[n + 1]),
+      M_current_loop(n > 0 ? 1 : 0),
+      M_did_break(false)
+      {
+        M_counters[M_current_loop] = b;
+      }
 
     // Destructor.
     ~MultiLoop() { delete [] M_counters; }
@@ -94,17 +113,20 @@ class MultiLoop {
     // Return a reference to counter number i.
     int& operator[](unsigned int i) { assert((int)i < M_current_loop); return M_counters[i + 1]; }
 
-    // Return the value of the counter of the current inner loop (operator[](operator()())).
-    int operator()(void) const { return M_counters[M_current_loop]; }
+    // Return the value of the counter for the n-th previous loop.
+    int operator()(unsigned int n = 0) const { assert((int)n < M_current_loop); return M_counters[M_current_loop - n]; }
 
-    // Advance the counters.
-    void operator++(void);
+    // Set the loop counter to value n.
+    void operator=(int n) { M_counters[M_current_loop] = n; }
+
+    // Advance the counters. Start the next loop with value b.
+    void start_next_loop_at(int b = 0);
 
     // Break out of the current loop and increment previous loop.
-    void next_loop(void) { ++M_counters[--M_current_loop]; }
+    void next_loop(void) { ++M_counters[--M_current_loop]; M_did_break = false; }
 
-    // On the next call to next(), break out of n loops.
-    void breaks(int n) { M_current_loop -= (n - 1); assert(M_current_loop > 0); }
+    // On the next call to start_next_loop_at(), break out of n loops.
+    void breaks(int n) { M_did_break = M_current_loop == (int)M_loops; M_current_loop -= (n - 1); assert(M_current_loop > 0); }
 
     // Return true when all loops are finished.
     bool finished(void) const { return M_current_loop == 0; }
@@ -112,19 +134,23 @@ class MultiLoop {
     // Return true when we are in the inner loop.
     bool inner_loop(void) const { return M_current_loop == (int)M_loops; }
 
+    // Return true when we're at the end of a loop (but not the inner loop).
+    bool end_of_loop() const { return !M_did_break && M_current_loop > 1; }
+
   private:
     unsigned int  M_loops;
     int*          M_counters;
     int           M_current_loop;
+    bool          M_did_break;
 };
 
-inline void MultiLoop::operator++(void)
+inline void MultiLoop::start_next_loop_at(int b)
 {
   // If we are not in the inner loop, start a next loop.
   if (M_current_loop < (int)M_loops)
   {
     ++M_current_loop;
-    M_counters[M_current_loop] = 0;
+    M_counters[M_current_loop] = b;
   }
   else
   {
