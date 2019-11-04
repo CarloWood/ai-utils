d2f32ac444ea51a4791f31d702640fbd4aa5fa54
Carlo Wood <carlo@alinoe.com>
2019-11-04 01:21:39 +0100

    Improved print_using - now also accepting member functions and overloaded function.

--- print_using.h
+++ print_using.h
@@ -30,31 +30,38 @@
 
 namespace utils {
 
-template<typename T, typename L>
+template<typename T, typename print_on_type>
 class PrintUsing2
 {
  private:
   T m_obj;
-  L const m_print_on;
+  print_on_type m_print_on;
 
  public:
-  PrintUsing2(T obj, L print_on) : m_obj(obj), m_print_on(print_on) { }
+  PrintUsing2(T obj, print_on_type print_on) : m_obj(obj), m_print_on(print_on) { }
 
-  friend std::ostream& operator<<(std::ostream& os, PrintUsing2 const& print_using)
+  friend std::ostream& operator<<(std::ostream& os, PrintUsing2 print_using)
   {
-    print_using.m_print_on(os, print_using.m_obj);
+    Dout(dc::notice, "Using " << libcwd::type_info_of<PrintUsing2>().demangled_name());
+    if constexpr (std::is_member_function_pointer_v<print_on_type>)
+      (print_using.m_obj.*print_using.m_print_on)(os);
+    else
+      print_using.m_print_on(os, print_using.m_obj);
     return os;
   }
 };
 
-template<typename L>
+template<typename T> using PrintUsing2_by_value = PrintUsing2<T, void(*)(std::ostream&, T)>;
+template<typename T> using PrintUsing2_by_const_reference = PrintUsing2<T, void(*)(std::ostream&, T const&)>;
+template<typename T> using PrintUsing2_by_const_member_function = PrintUsing2<T, void(T::*)(std::ostream&) const>;
+
 class PrintUsing1
 {
  private:
-  L const m_print_on;
+  void (*m_print_on)(std::ostream&);
 
  public:
-  PrintUsing1(L print_on) : m_print_on(print_on) { }
+  PrintUsing1(void (*print_on)(std::ostream&)) : m_print_on(print_on) { }
 
   friend std::ostream& operator<<(std::ostream& os, PrintUsing1 const& print_using)
   {
@@ -63,14 +70,25 @@ class PrintUsing1
   }
 };
 
-template<typename T, typename L>
-PrintUsing2<T, L> print_using(T obj, L print_on)
+template<typename T>
+PrintUsing2_by_value<T> print_using(T obj, void (*print_on)(std::ostream&, T))
 {
   return { obj, print_on };
 }
 
-template<typename L>
-PrintUsing1<L> print_using(L print_on)
+template<typename T>
+PrintUsing2_by_const_reference<T> print_using(T obj, void (*print_on)(std::ostream&, T const&))
+{
+  return { obj, print_on };
+}
+
+template<typename T>
+PrintUsing2_by_const_member_function<T> print_using(T obj, void (T::*print_on)(std::ostream&) const)
+{
+  return { obj, print_on };
+}
+
+PrintUsing1 print_using(void (*print_on)(std::ostream&))
 {
   return { print_on };
 }
