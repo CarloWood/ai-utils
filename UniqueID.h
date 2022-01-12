f1aa6226d1694a23dd1fa74e5341767941563718
Carlo Wood <carlo@alinoe.com>
2022-01-12 01:57:38 +0100

    Support non-integral types for UniqueID, albeit not thread-safe.

--- UniqueID.h
+++ UniqueID.h
@@ -6,28 +6,58 @@
 
 namespace utils {
 
-template<std::integral T>
+template<typename T>
 class UniqueIDContext;
 
-template<std::integral T>
+template<typename Q>
+concept SaneUniqueIDT = sizeof(Q) <= 2 * sizeof(long);
+
+template<typename Q>
+concept LargeUniqueIDT = sizeof(Q) > 2 * sizeof(long);
+
+template<typename T>
 class UniqueID
 {
  private:
   T const m_id;
 
  public:
-  UniqueID(Badge<UniqueIDContext<T>>, T id) : m_id(id) { }
+  UniqueID(Badge<UniqueIDContext<T>>, T id) : m_id(std::move(id)) { }
 
-  operator T() const { return m_id; }
-  bool operator==(UniqueID other) const { return m_id == other.m_id; }
-  bool operator<(UniqueID other) const { return m_id < other.m_id; }
+  // By value - the size of T is expected to be small under normal circumstances.
+  template<SaneUniqueIDT Q = T> operator T() const { return m_id; }
+  template<SaneUniqueIDT Q = T> friend bool operator==(UniqueID lhs, UniqueID rhs) { return lhs.m_id == rhs.m_id; }
+  template<SaneUniqueIDT Q = T> friend bool operator<(UniqueID lhs, UniqueID rhs) { return lhs.m_id < rhs.m_id; }
+
+  // And well...
+  template<LargeUniqueIDT Q = T> operator T const&() const { return m_id; }
+  template<LargeUniqueIDT Q = T> friend bool operator==(UniqueID const& lhs, UniqueID const& rhs) { return lhs.m_id == rhs.m_id; }
+  template<LargeUniqueIDT Q = T> friend bool operator<(UniqueID const& lhs, UniqueID const& rhs) { return lhs.m_id < rhs.m_id; }
 };
 
 template<std::integral T>
-class UniqueIDContext
+class UniqueIDContext<T>
 {
  private:
+  static constexpr bool thread_safe = true;
   std::atomic<T> m_next_id;
+
+ public:
+  UniqueIDContext() = default;
+
+  UniqueID<T> get_id()
+  {
+    return { {}, m_next_id++ };
+  }
+};
+
+// Non-integral version is not thread-safe!
+template<typename T> requires (!std::integral<T>)
+class UniqueIDContext<T>
+{
+ private:
+  static constexpr bool thread_safe = false;
+  T m_next_id;
 
  public:
   UniqueIDContext() = default;
