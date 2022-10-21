aae4676c435640fcd612f5d297212da9755c8078
Carlo Wood <carlo@alinoe.com>
2022-10-21 20:43:01 +0200

    Add class Index.

---
+++ Index.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <concepts>
+
+namespace utils {
+
+template<typename Category, std::integral INT>
+class Index
+{
+ private:
+  INT m_index;
+
+ public:
+  constexpr explicit Index(INT index) : m_index(index) { }
+  INT get_value() const { return m_index; }
+  Index& operator++()    { ++m_index; return *this; }
+  Index& operator--()    { --m_index; return *this; }
+  Index  operator++(int) { Index prev(m_index); ++m_index; return prev; }
+  Index  operator--(int) { Index prev(m_index); --m_index; return prev; }
+  auto operator<=>(Index rhs) { return m_index <=> rhs.m_index; }
+  bool operator==(Index const&) const = default;
+};
+
+} // namespace utils
