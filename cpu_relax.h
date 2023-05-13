80b6dea1077981f791338f1c1223568d1c100503
Long Wong <efylw5@exmail.nottingham.ac.uk>
2023-05-13 17:06:56 +0000

    Supporting M1 devices for cpu_relax.h

--- cpu_relax.h
+++ cpu_relax.h
@@ -31,6 +31,8 @@
 {
 #if defined(__x86_64__) || defined(__i386__)
   asm volatile("pause" ::: "memory");
+#elif defined(__aarch64__)
+  asm volatile("yield" ::: "memory");
 #else
 #error "Please extent utils/cpu_relax.h for your architecture."
 #endif
