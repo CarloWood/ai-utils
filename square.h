c4d47a8f08cf23b65c3c892c85e253175a0bc0a0
Carlo Wood <carlo@alinoe.com>
2023-12-30 04:28:49 +0100

    Add utils::square.

---
+++ square.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <concepts>
+#include <type_traits>
+
+namespace utils {
+
+template<typename T>
+concept ConceptMultiplicableAssignable = requires(T a, T b)
+{
+  { a *= b } -> std::convertible_to<T>;
+};
+
+template<typename T>
+concept ConceptBuiltin = std::is_integral_v<T> || std::is_floating_point_v<T>;
+
+// Returns the square of its argument.
+template<typename T>
+requires ConceptMultiplicableAssignable<T> && (!ConceptBuiltin<T>)
+T square(T const& arg)
+{
+  T result(arg);
+  result *= arg;
+  return result;
+}
+
+template<ConceptBuiltin T>
+T square(T arg)
+{
+  return arg * arg;
+}
+
+} // namespace utils
