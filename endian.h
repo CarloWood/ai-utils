e81d44c5f2e0b0921959252beb8d860d069a3619
Carlo Wood <carlo@alinoe.com>
2025-08-14 13:19:36 +0200

    Move uint*_to_be, uint*_to_le, be_to_uint* and le_to_uint* into namespace utils.
    This breaks API - just add `utils::` in front of the above.
    Also added ntoh and hton.

--- endian.h
+++ endian.h
@@ -1,11 +1,19 @@
 // This is a copy of https://github.com/alipha/cpp/blob/8c4313a8ba131e4822c5adcd09e62dc2b2ab9c3d/endian/endian.hpp
 // See https://github.com/alipha/cpp/blob/8c4313a8ba131e4822c5adcd09e62dc2b2ab9c3d/LICENSE for the license.
 
-#ifndef LIPH_ENDIAN_HPP
-#define LIPH_ENDIAN_HPP
+// Change log
+//
+// 2025/08/14
+// - Put everything in namespace utils.
+// - Added utils::ntoh and utils::hton.
+// - Use #pragma once instead of macro guard.
+
+#pragma once
 
 #include <cstdint>
+#include <type_traits>
 
+namespace utils {
 
 /* gcc 8.1 and clang 9.0.0 and above optimize these to nops or bswaps with -O2.
    However, le_to_uint16 and be_to_uint16 are not optimal on gcc 8.1-9.3, but
@@ -150,4 +158,15 @@ std::uint16_t le_to_uint16(const Char *src) {
     );
 }
 
-#endif
+// Reworked from https://stackoverflow.com/a/77937931/1487069
+template<typename T> std::enable_if_t<sizeof(T) == 1, T> ntoh(T v) { return v; }
+template<typename T> std::enable_if_t<sizeof(T) == 2, T> ntoh(T v) { return be_to_uint16(reinterpret_cast<char const*>(&v)); }
+template<typename T> std::enable_if_t<sizeof(T) == 4, T> ntoh(T v) { return be_to_uint32(reinterpret_cast<char const*>(&v)); }
+template<typename T> std::enable_if_t<sizeof(T) == 8, T> ntoh(T v) { return be_to_uint64(reinterpret_cast<char const*>(&v)); }
+
+template<typename T> std::enable_if_t<sizeof(T) == 1, T> hton(T v) { return v; }
+template<typename T> std::enable_if_t<sizeof(T) == 2, T> hton(T v) { T result; uint16_to_be(v, &result); return result; }
+template<typename T> std::enable_if_t<sizeof(T) == 4, T> hton(T v) { T result; uint32_to_be(v, &result); return result; }
+template<typename T> std::enable_if_t<sizeof(T) == 8, T> hton(T v) { T result; uint64_to_be(v, &result); return result; }
+
+} // namespace utils
