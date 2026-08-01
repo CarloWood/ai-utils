2c35580794a77a9106e47743803dc9867ff1c6c4
Carlo Wood <carlo@alinoe.com>
2026-08-01 14:09:24 +0200

    Drop support for GNU autotools. Update documentation to refer to cmake-aicxx instead of cwm4.

--- Global.h
+++ Global.h
@@ -134,7 +134,7 @@
 #include "debug.h"
 #if defined(DEBUGGLOBAL) && (!defined(CWDEBUG) || !CWDEBUG_ALLOC || !CWDEBUG_LOCATION)
 #ifdef CWDEBUG
-#error "Please configure with --disable-debug-global / -DEnableDebugGlobal:BOOL=OFF or (re)configure libcwd with --enable-alloc --enable-location / -DEnableLibcwdAlloc:BOOL=ON -DEnableLibcwdLocation:BOOL=ON (in cmake/gitache-configs/libcwd_r.cmake)."
+#error "Please configure with -DEnableDebugGlobal:BOOL=OFF or (re)configure libcwd without -DEnableLibcwdLocation:BOOL=OFF (in cmake/gitache-configs/libcwd.cmake)."
 #else
 #error "You cannot define DEBUGGLOBAL without having CWDEBUG, CWDEBUG_ALLOC and CWDEBUG_LOCATION defined."
 #endif
